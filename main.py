9a9765c88a8782b0cdf2d789c6fb2cf0b19c380a
Pascal Ackermann <ackermann.pascal@gmail.con>
2023-04-26 08:12:28 +0200

    Added exception for KeyErorr

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pandas
 
+
+def translate_to_phonetic():
+    try:
+        user_input = input("Type your word: ").upper()
+        output_phonetic_code_list = [nato_phonetic_alphabet_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Only type letters please")
+        translate_to_phonetic()
+    else:
+        print(output_phonetic_code_list)
+
+
 # TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 nato_phonetic_alphabet_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic_alphabet_dict = {row.letter: row.code for (index, row) in nato_phonetic_alphabet_df.iterrows()}
-print(nato_phonetic_alphabet_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Type your word: ").upper()
-output_phonetic_code_list = [nato_phonetic_alphabet_dict[letter] for letter in user_input if
-                             letter in nato_phonetic_alphabet_dict.keys()]
-print(output_phonetic_code_list)
+translate_to_phonetic()
